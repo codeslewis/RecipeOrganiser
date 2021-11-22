a842ce8ef3590d0bb79afbf7e8f3b331aac72b6b
Lewis Sneddon <lewis@lewiscodes.io>
2021-11-22 09:51:20 +0000

    setup arango repository

--- RecipeOrganiser/Repositories/ArangoRecipeRepository.cs
+++ RecipeOrganiser/Repositories/ArangoRecipeRepository.cs
@@ -1,14 +1,33 @@
 ﻿using RecipeOrganiser.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using static RecipeOrganiser.Repositories.DbConnectionInfo;
+
+using ArangoDBNetStandard;
+using ArangoDBNetStandard.Transport.Http;
 
 namespace RecipeOrganiser.Repositories
 {
     class ArangoRecipeRepository : IRecipeRepository
     {
+        private readonly HttpApiTransport _transport;
+        //private readonly DatabaseApiClient _databaseApiClient;
+        private readonly ArangoDBClient _dbClient;
+
+        public ArangoRecipeRepository()
+        {
+            _transport = HttpApiTransport.UsingBasicAuth(
+                new Uri(URI),
+                DB_NAME,
+                USER_NAME,
+                PASSWORD);
+            _dbClient = new ArangoDBClient(_transport);
+        }
+
+        ~ArangoRecipeRepository()
+        {
+            _transport.Dispose();
+            _dbClient.Dispose();
+        }
+
         public void Add(Recipe recipe)
         {
             throw new NotImplementedException();
