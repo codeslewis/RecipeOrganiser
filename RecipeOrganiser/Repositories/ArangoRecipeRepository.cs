28040fda1cf70119c0f8286985a0ab9d6cbd78b8
Lewis Sneddon <lewis@lewiscodes.io>
2021-11-22 09:33:37 +0000

    local arango instance connection info

---
+++ RecipeOrganiser/Repositories/ArangoRecipeRepository.cs
@@ -0,0 +1,22 @@
+﻿using RecipeOrganiser.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RecipeOrganiser.Repositories
+{
+    class ArangoRecipeRepository : IRecipeRepository
+    {
+        public void Add(Recipe recipe)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Recipe> GetAll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
